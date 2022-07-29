8e908ee6c22c4b335321dba95ef78b9cd724ff38
Martin Parizek <marti.parizek@gmail.com>
2022-07-29 14:12:27 +0200

    first lesson done, add to collection, get data from collection

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+
+export const App = () => (
+  <div>
+    <h1>Meteor Wallet</h1>
+    <ContactForm/>
+    <ContactList/>
+  </div>
+);
